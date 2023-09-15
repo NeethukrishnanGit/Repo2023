762f182464dda52cf6f43c83274cfb6a59c77f88
ANORA\neethukrishnan <neethukrishnan@anoralabs.com>
2023-09-15 15:10:47 +0530

    white space added for choice

--- choice.py
+++ choice.py
@@ -1,6 +1,6 @@
 def circle(radius):
-    area=3.14*radius*radius
-    print("Area of circle is: ",area)
+    area = 3.14*radius*radius
+    print("Area of circle is: ", area)
 
 def rectangle(length,breadth):
     area=length*breadth
@@ -19,17 +19,17 @@ while True:
 
     choice=int(input("Enter the choice: "))
 
-    if choice==1:
+    if choice == 1:
         radius=int(input("Enter the radius: "))
         circle(radius)
-    elif choice==2:
+    elif choice == 2:
         length=int(input("Enter the length: "))
         breadth=int(input("Enter the breadth: "))
         rectangle(length,breadth)
-    elif choice==3:
+    elif choice == 3:
         side=int(input("Enter the side: "))
         square(side)
-    elif choice==4:
+    elif choice == 4:
         break
     else:
         print("Incorrect input")
